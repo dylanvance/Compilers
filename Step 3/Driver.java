c8b5fd45b4f1feec22eaba913e551116dd560966
Dylan Vance <95720477+dylanvance@users.noreply.github.com>
2024-02-26 12:35:53 -0500

    Create Driver.java

---
+++ Step 3/Driver.java
@@ -0,0 +1,36 @@
+import org.antlr.v4.runtime.*;
+import org.antlr.v4.runtime.tree.*;
+import org.antlr.v4.*;
+import java.util.*;
+
+public class Driver {
+	
+	public static void main(String args[]) throws Exception {
+		// Create a CharStream that reads from standard input
+		ANTLRInputStream input = new ANTLRInputStream(System.in);
+
+		// Create a lexer that feeds off of input CharStream
+		LittleLexer lexer = new LittleLexer(input);
+
+		// Create a buffer of tokens pulled from the lexer
+		CommonTokenStream tokens = new CommonTokenStream(lexer);
+
+		// Create a parser that feeds off the tokens buffer
+		LittleParser parser = new LittleParser(tokens);
+
+		// Begin parsing at prog rule
+		ParseTree tree = parser.program();
+
+		// Create a generic parse tree walker that can trigger callbacks
+		ParseTreeWalker walker = new ParseTreeWalker();
+
+		// Create the parse table
+		SimpleTableBuilder stb = new SimpleTableBuilder();
+		
+		// Walk the tree created during the parse, trigger callbacks
+		walker.walk(stb, tree);
+		
+		// Print the symbol table
+		stb.prettyPrint();
+	}
+}
